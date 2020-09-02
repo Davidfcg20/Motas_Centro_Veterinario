7c4011d6c288943a59c9b52d9b579de3297f316b
Marianyelicardenas <Marianyeliurrieta@gmail.com>
2020-09-02 15:07:08 -0500

    entrega tres

--- index.php
+++ index.php
@@ -94,6 +94,17 @@
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/hB7mRQPaJEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
+<br>
+<br>
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FP%25C3%25A1gina-de-adopci%25C3%25B3n-perros-y-gatos-312796346252551%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
+<br>
+<br>
+<br>
+
+<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcachorrosmedellin.dogscan%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
 </body>
 </html>
+
 
