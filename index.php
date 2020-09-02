5604144b714a3920e1082ac8c6c0795eb40f60a8
Davidfcg20 <david.felipecg20@gmail.com>
2020-09-02 01:30:13 -0500

    Primer parte

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CENTRO VETERINARIO MOTAS</title>
+</head>
+<body>
+	<?php  ?>
+	<p> Hola Mary :D </p>
+
+	<a href="https://www.tdea.edu.co" target="_blank">Página del TdeA en nueva pestaña</a>
+
+</body>
+</html>
