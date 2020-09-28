62c6656013cdf8a79541a1a369e0ed80c57541ce
Davidfcg20 <david.felipecg20@gmail.com>
2020-09-27 21:19:10 -0500

    trabajo 3

--- index.php
+++ index.php
@@ -2,106 +2,84 @@
 <html>
 <head>
 	<title>CENTRO VETERINARIO MOTAS</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
 </head>
-<body>
+
 
 	<?php  ?>
 
+	<header>
+	<img src="Imagenes/LogoPagina.png">
+		<nav>
+			<ul id="menu">
+				<li><a href=""><strong>Inicio</strong></a></li>
+				<li><a href="">Servicios</a>
+					<ul>
+						<li><a href="">Consulta Veterinaria</a>
+						<li><a href="">Peluqueria</a>
+						<li><a href="">Vacunas</a>
+					</ul>
+						</li>
+				<li><a href="">Galeria</a></li>
+				<li><a href="">Nosotros</a></li>
+				<li><a href="">Contactanos</a></li>
+			</ul>
+		</nav>
+	</header><br>
 
-	<H1>Motas Centro Veterinario</H1>
-		<p style="text-align: justify; margin-left:30px; margin-right: 40px;">La página de Motas Centro Veterinario se plantea como un proyecto donde se apliquen los aprendizajes de las materias Base de Datos e Ingeniería del Software, este abarca aspectos como la presentación y le da importancia a la parte estética, optimizando la imagen del Centro Veterinaria Motas; para lo que tendrá una presentación breve, menú desplegable con los servicios ofrecidos, datos de contactos, galería de imágenes y noticias del centro veterinario, proyectando a futuro una base de datos que permita al usuario del servicio acceder para consultar la historia clínica de su mascota.</p>
+		<div id="divirin"><img id="pepe" src="Imagenes/Banner.png"></div><br>
 
-		<i><p style="text-align: justify; margin-left:30px; margin-right: 40px;">Los colores Blanco y Azul son emblemáticos del Centro Veterinario Motas, por lo que se tendrán en cuenta al realizar el proyecto web. </p></i><br>
+		<div class="div">
 
-		<img src="Imagenes/Logo.png" alt="Imagen" width="40%"><img>
 
-	<H1>Registro por primera vez</H1>
+		<h2>SERVICIOS</h2>
 
-		<form action="otra.html" method="get">
+		<div id="servgen">
+
+		<div class="servesp"><a href="http://google.com"><img class="rounded-circle" alt="140x140" style="width: 140px; height: 140px;" src="Imagenes/BotonAnimalito.png" data-holder-rendered="true"></a><p>Consultas Generales</p></div>
+		<div class="servesp"><a href="http://google.com"><img class="rounded-circle" alt="140x140" style="width: 140px; height: 140px;" src="Imagenes/BotonAnimalito.png" data-holder-rendered="true"></a><p>Cirugía Especializada</p></div>
+		<div class="servesp"><a href="http://google.com"><img class="rounded-circle" alt="140x140" style="width: 140px; height: 140px;" src="Imagenes/BotonAnimalito.png" data-holder-rendered="true"></a><p>Vacuna y desparasitación</p></div>
+		<div class="servesp"><a href="http://google.com"><img class="rounded-circle" alt="140x140" style="width: 140px; height: 140px;" src="Imagenes/BotonAnimalito.png" data-holder-rendered="true"></a><p>Peluquería</p></div>
+
+		</div>
+
+		</div><br>
+
+		<div class="div">
+
+
+		<h2>GALERIA</h2>
+
+		<div class="servgen2">
+
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria1.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria2.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria3.png"data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria4.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+
+		</div>
+
+		<div class="servgen2">
+
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria5.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria6.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria7.png"data-holder-rendered="true"><p>Consultas Generales</p></div>
+		<div class="servesp2"><img class="square" alt="300x150" style="width: 290px; height: 150px;" src="Imagenes/Galeria8.png" data-holder-rendered="true"><p>Consultas Generales</p></div>
+
+		</div>
+
+		</div><br>
+
+		<div class="div">
+
+
+		<h2>NOTICIAS</h2>
+
+		<div class="servgen3">
+
 		
-		<label>Nombre Propietario:</label><br>
-		<input type="text" name="nombre" placeholder="David Cataño G" required><br>
 
-		<label>Identificación Propietario:</label><br>
-		<input type="number" name="cedula" placeholder="1000189091"><br>
-
-		<label>Tipo de Mascota:</label><br>
-		<select name="Tipo">
-			<option></option>
-			<option>Gato</option>
-			<option>Perro</option>
-			<option>Pajaro</option> 
-		</select><br>
-
-		<label>Nombre Mascota:</label><br>
-		<input type="text" name="nombre" placeholder="Pecas" required><br>
-
-		<label>Teléfono:</label><br>
-		<input type="tel" name="telefono" placeholder="3008584xxx" ><br>
-
-		<label>Correo:</label><br>
-		<input type="email" name="email" placeholder="david.felipecg20@xxxx.xxx"><br>
-
-		<label>Seleccione una Contraseña:</label><br>
-		<input type="password" name="contrasena"><br>
-
-		<label>Repita la Contraseña:</label><br>
-		<input type="password" name="contrasena"><br>
-		<br>
-
-		<button type="submit">ENVIAR</button><br>
-
-		</form><br>
-
-		<table>
-
-		<caption>Productos</caption>
-
-		<tr>
-			<th>Nombre Del Producto</th>
-			<th><input type="text" name="nombre" placeholder="Producto " required></th>
-		</tr>
-
-			<th>Cantidad De Producto</th>
-			<th><input type="text" name="nombre" placeholder="1 " required></th>
-
-		<tr>
-		<th>Referencia Del Producto</th>
-			<th><input type="text" name="nombre" placeholder=" " required></th>
-		</tr>
-		
-		</table><br>
-
-		<hr>
-
-	<H2 style="text-align: center;">SITIOS INTERNOS Y EXTERNOS</H2>	
-
-	<i><H3>Presione los botones para interactuar</H3></i>
-			
-			<a href="Fotos.html"><button type="submit">FOTOS VETERINARIO</button><br></a><br>
-
-			<a href="http://google.com"><img class="rounded-circle" alt="140x140" style="width: 140px; height: 140px;" src="Imagenes/BotonAnimalito.png" data-holder-rendered="true"></a><br>
-			<br>
-
-	<i><H3>Sitios de interés</H3></i><br>
-
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/9dDemBxxulo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
-
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/hB7mRQPaJEw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
-			<br>
-
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FP%25C3%25A1gina-de-adopci%25C3%25B3n-perros-y-gatos-312796346252551%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe><br>
-			<br>
-			<br>
-
-			<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcachorrosmedellin.dogscan%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe><br>
-
+		<div id="servesp3">
 			<blockquote class="twitter-tweet"><p lang="es" dir="ltr">La FAO identifica lagunas de conocimiento sobre Covid-19 y animales <a href="https://t.co/klQPIunUQD">https://t.co/klQPIunUQD</a></p>&mdash; Animal&#39;s Health (@animalshealthes) <a href="https://twitter.com/animalshealthes/status/1286321435909197826?ref_src=twsrc%5Etfw">July 23, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-
-
-</body>
-</html>
-
-
-
-
+			<p>Aqui va el texto de la primer noticia</p></div>
