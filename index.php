fcc2e77c52d49dcb0e65ae857bcaa43a2260c2e8
Marianyelicardenas <Marianyeliurrieta@gmail.com>
2020-09-27 22:11:41 -0500

    cuartaentrega

--- index.php
+++ index.php
@@ -83,3 +83,92 @@
 		<div id="servesp3">
 			<blockquote class="twitter-tweet"><p lang="es" dir="ltr">La FAO identifica lagunas de conocimiento sobre Covid-19 y animales <a href="https://t.co/klQPIunUQD">https://t.co/klQPIunUQD</a></p>&mdash; Animal&#39;s Health (@animalshealthes) <a href="https://twitter.com/animalshealthes/status/1286321435909197826?ref_src=twsrc%5Etfw">July 23, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 			<p>Aqui va el texto de la primer noticia</p></div>
+
+			<div id="servesp4">
+			<p>Aqui va el texto de la segunda noticia</p><iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcachorrosmedellin.dogscan%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe></div>
+
+		</div>
+
+		</div><br>
+
+		
+		<div>
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d63457.366780586206!2d-75.6136583!3d6.2524749!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4428ede76e32b5%3A0x802587e255b201e3!2sCl.%2066%2C%20Medell%C3%ADn%2C%20Antioquia%2C%20Colombia!5e0!3m2!1ses!2sve!4v1601225150202!5m2!1ses!2sve" frameborder="0" style="border:0;" allowfullscreen aria-hidden="false" tabindex="0"></iframe>
+      	</div><br>
+
+      <!--- AQUI PEGAMOS EL EJEMPLO --->
+
+
+		<div class="div">
+
+		<section id="appointment" class="appointment section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Haga una Cita</h2>
+        
+        </div>
+
+        <form action="forms/appointment.php" method="post" role="form" class="php-email-form" data-aos="fade-up" data-aos-delay="100">
+          <div class="form-row">
+            <div class="col-md-4 form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder=Nombre data-rule="minlen:4" data-msg="Por favor Escriba al menos 4 caracteres">
+              <div class="validate"></div>
+            </div>
+            <div class="col-md-4 form-group">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Su correo electrónico" data-rule="email" data-msg="Por favor ingrese un correo valido">
+              <div class="validate"></div>
+            </div>
+            <div class="col-md-4 form-group">
+              <input type="tel" class="form-control" name="phone" id="phone" placeholder="Su teléfono" data-rule="minlen:4" data-msg="Por favor Escriba al menos 4 caracteres">
+              <div class="validate"></div>
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="col-md-4 form-group">
+              <input type="datetime" name="date" class="form-control datepicker" id="date" placeholder="Fecha de la Cita" data-rule="minlen:4" data-msg="Por favor Escriba al menos 4 caracteres">
+              <div class="validate"></div>
+            </div>
+            <div class="col-md-4 form-group">
+              
+              <div class="validate"></div>
+            </div>
+            <div class="col-md-4 form-group">
+              
+              <div class="validate"></div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <textarea class="form-control" name="message" rows="5" placeholder="Mensaje (Opcional)"></textarea>
+            <div class="validate"></div>
+          </div>
+          <div class="mb-3">
+            <div class="loading">Cargando</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Su cita ha sido enviada satisfactoriamente. Gracias!</div>
+          </div>
+          <div class="text-center"><button type="submit">Haga una Cita</button></div>
+        </form>
+
+      </div>
+    </section><!-- Fin seccion de cita -->
+
+    	</div>
+
+
+
+
+
+		<!--- AQUI PEGAMOS EL EJEMPLO --->
+
+		<footer><img src="Imagenes/Footer.png"></footer>
+		
+	
+
+</body>
+</html>
+
+
+
+
